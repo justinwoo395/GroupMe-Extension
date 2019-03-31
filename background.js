6bdb451803577e8a76d5f83ee2ae2413179cc4a0
= <=>
2019-03-31 00:30:41 -0700

    fixed toggle behaviors

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 // chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#000000'}, function() {
-      console.log("got jhere");
-      $('img[src*="large"]').attr("src", "https://pixel.nymag.com/imgs/daily/intelligencer/2018/09/24/24-bongo-cat.w700.h700.jpg");
+    
    
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
@@ -13,6 +12,10 @@
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
-    console.log("got jhere");
-    $('img[src*="large"]').attr("src", "https://pixel.nymag.com/imgs/daily/intelligencer/2018/09/24/24-bongo-cat.w700.h700.jpg");
+    console.log("got here");
+    
+    
+
  // });
+
+
