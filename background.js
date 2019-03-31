7e32331a9554357edc320bb485c1f06152ac0b1f
Justin Woo <justinwoo395@gmail.com>
2019-03-30 22:47:15 -0700

    First commit

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#000000'}, function() {
+      console.log("The color is green.");
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'web.groupme.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+
+    $('img[src*="large"]').attr("src", "https://pixel.nymag.com/imgs/daily/intelligencer/2018/09/24/24-bongo-cat.w700.h700.jpg");
+ });
+
