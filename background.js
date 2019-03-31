75973ce295eff6f4fd49abf0eed579e683447573
Kallan Bao <kallanbao@berkeley.edu>
2019-03-31 02:32:28 -0700

    added adblocker with groupme stuff

--- background.js
+++ background.js
@@ -1,18 +1,25 @@
-// chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#000000'}, function() {
-      console.log("got jhere");
-      $('img[src*="large"]').attr("src", "https://pixel.nymag.com/imgs/daily/intelligencer/2018/09/24/24-bongo-cat.w700.h700.jpg");
-   
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'web.groupme.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-    console.log("got jhere");
-    $('img[src*="large"]').attr("src", "https://pixel.nymag.com/imgs/daily/intelligencer/2018/09/24/24-bongo-cat.w700.h700.jpg");
- // });
+
+var adCount = 0;
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function logging(details) {
+    adCount += 1;
+		console.log("blocking:", details.url);
+    console.log("# of ads yoten:" + adCount);
+		return {cancel: true };
+	},
+	{urls: blocked_domains},
+	["blocking"]
+);
+
+chrome.storage.sync.set({color: '#000000'}, function() {
+});
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {hostEquals: 'web.groupme.com'},
+    })
+    ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});
