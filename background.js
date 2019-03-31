0001430361024be95f1ee1e928b07a58d034c3aa
= <=>
2019-03-31 15:04:56 -0700

    updated options

--- background.js
+++ background.js
@@ -1,21 +1,13 @@
-
-// chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#000000'}, function() {
-   
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'web.groupme.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-    console.log("got here");
- // });
-
-
+/*chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {hostEquals: 'https://*.groupme.com/*'},
+    })
+    ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+});*/
+ 
 chrome.runtime.onMessage.addListener(
   function(message, sender, response) {
     console.log('received');
